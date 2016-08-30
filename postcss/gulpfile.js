fa069ee4923defdb8acacc66efd0d15c4655512c
dantehemerson <dantehemerson@gmail.com>
2016-08-30 12:53:39 -0500

    Add nested prefixer

--- postcss/gulpfile.js
+++ postcss/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 var postcss = require('gulp-postcss')
 var autoprefixer = require('autoprefixer')
+var cssnested = require('postcss-nested')
 var browserSync = require('browser-sync').create()
 
 // Servidor de desarrollo
@@ -17,7 +18,8 @@ gulp.task('css', function() {
 	var processors = [
 		autoprefixer({
 			browsers: ['> 5%', 'ie 8']
-		})
+		}),
+		cssnested
 	]
 	return gulp.src('./src/*.css')
 		.pipe(postcss(processors))
