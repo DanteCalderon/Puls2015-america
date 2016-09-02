46fadc6b510b1c151c3a82261e5764867ad693a2
dantehemerson <dantehemerson@gmail.com>
2016-09-02 15:43:42 -0500

    Using mixins

--- postcss/gulpfile.js
+++ postcss/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp')
 var postcss = require('gulp-postcss')
 var cssnext = require('postcss-cssnext')
 var cssnested = require('postcss-nested')
+var mixins = require('postcss-mixins')
 var browserSync = require('browser-sync').create()
 
 // Servidor de desarrollo
@@ -15,8 +16,9 @@ gulp.task('serve', function() {
 
 // Tarea para procesar el css
 gulp.task('css', function() {
-	var processors = [	
-		cssnested,
+	var processors = [			
+		mixins(),
+		cssnested,		
 		cssnext({
 			browsers: ['> 5%', 'ie 8']
 		})
